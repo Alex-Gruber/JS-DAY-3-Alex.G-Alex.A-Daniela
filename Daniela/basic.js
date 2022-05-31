ce23ce515591b954c3bc4fe96ac8b60422d83475
ajnacodes <b.daniela196@outlook.com>
2022-05-31 13:40:13 +0200

    basic3 done

--- Daniela/basic.js
+++ Daniela/basic.js
@@ -1,18 +1,52 @@
-Math.random();
+// Math.random();
 
-var therandomNumber = Math.floor(Math.random() * 31) - 5;
+// var therandomNumber = Math.floor(Math.random() * 31) - 5;
 
-// console.log(therandomNumber)
-if (therandomNumber < 10) {
-    console.log('The weather is cold')
-} else if (therandomNumber < 16) {
-    console.log('The weather is moderate')
-} else {
-    console.log('The weather is warm')
+// // console.log(therandomNumber)
+// if (therandomNumber < 10) {
+//     console.log('The weather is cold')
+// } else if (therandomNumber < 16) {
+//     console.log('The weather is moderate')
+// } else {
+//     console.log('The weather is warm')
+// }
+
+
+// // Basic 2
+
+// let highestnumber = [2, 5, -12, -14, 3, 15, 2345, 545454, 2]
+// console.log(Math.max(...highestnumber))
+
+
+// Basic 3
+
+
+
+
+
+
+function myFunction() {
+    var randomNumber = Math.floor(Math.random() * 41) - 5;
+
+    console.log(randomNumber)
+    if (randomNumber < 10) {
+        console.log('The weather is cold')
+    } else if (randomNumber < 25) {
+        console.log('The weather is moderate')
+    } else {
+        console.log('The weather is hotttt')
+    }
+
+
+    if (randomNumber < 11) {
+        document.getElementById('moderate').style.display = "none";
+        document.getElementById('hot').style.display = "none";
+    } else if (randomNumber < 25) {
+        document.getElementById('cold').style.display = "none";
+        document.getElementById('hot').style.display = "none";
+    } else {
+        document.getElementById('cold').style.display = "none";
+        document.getElementById('moderate').style.display = "none";
+    }
 }
-
-
-// Basic 2
-
-let highestnumber = [2, 5, -12, -14, 3, 15, 2345, 545454, 2]
-console.log(Math.max(...highestnumber))
+myFunction();
